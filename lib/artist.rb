df7194155dcdb28378128d8f758fc4807c1a1c37
Michael Fahim <michael_fahim@yahoo.com>
2020-01-25 17:46:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,46 +1,45 @@
 class Artist
-  attr_accessor :name, :songs, :artist
+  attr_accessor :name, :songs
 
-  @@all = []
+   @@all = []
 
-  def initialize(name)
+   def initialize(name)
     @name = name
     @songs = []
+    save
   end
 
-  def add_song(song)
-    @songs << song
-  end
-
-  def save
-    @@all << self
-  end
-
-
-  def self.all
+   def self.all
     @@all
   end
 
-
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
+   def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
   end
 
-
-
-  def self.find(name)
-    self.all.find { |artist| artist.name == name }
+   def self.find(name)
+    self.all.detect { |artist| artist.name == name }
   end
 
-
-  def self.create(name)
-    self.new(name).tap {|artist| artist.save}
+   def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
   end
 
-  def print_songs
-    songs.each do |song|
-      puts song.name
-    end
+   def add_song(song)
+    @songs << song
   end
 
-end
+   def print_songs
+    songs.each { |song| puts song.name }
+  end
+
+   def save
+    @@all << self
+  end
+
+   def songs
+    @songs
+  end
+end 
