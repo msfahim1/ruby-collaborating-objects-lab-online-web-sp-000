f80f831f13e39c3ed1904c46253adb51930e7243
Michael Fahim <michael_fahim@yahoo.com>
2020-01-25 17:48:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,19 @@
 class Song
   attr_accessor :name, :artist
 
-   def initialize(name)
+  @@all = []
+
+  def initialize(name)
     @name = name
+    @artist = artist 
+    @@all << self 
   end
 
-   def self.new_by_filename(file)
+  def self.all 
+    @@all
+  end
+
+ def self.new_by_filename(file)
     artist_name = file.split(" - ")[0]
     song_name = file.split(" - ")[1]
     song = Song.new(song_name)
@@ -13,9 +21,11 @@ class Song
     song
   end
 
-   def artist_name=(name)
+  def artist_name=(name)
     artist = Artist.find_or_create_by_name(name)
     self.artist = artist
     artist.add_song(self)
   end
-end 
+
+
+end  
